bba2780b218decdad1a51cb60eeb33f872e3133b
Nauman Umer <nmanumr@gmail.com>
2019-03-11 23:02:25 +0500

    Create q1.c

---
+++ labsheet3/q1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void q1()
+{
+    float liters, km, sum = 0, i = 0;
+
+    while (1)
+    {
+        printf("Enter the liters used (-1 to end): ");
+        scanf("%f", &liters);
+        if (liters == -1)
+            break;
+
+        printf("Enter the kilometers driven: ");
+        scanf("%f", &km);
+
+        float x = liters / km * 100.0;
+        sum += x;
+        i++;
+        printf("The liters/100 km for this tank was %.1f\n\n", x);
+    }
+
+    float avg = sum / i;
+
+    printf("The overall average consumption was: %.1f\n\n", avg);
+}
