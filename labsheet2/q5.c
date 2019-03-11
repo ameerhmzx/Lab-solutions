d750a4a6f3f231fa6c7d69b14c624a62932c9f53
Nauman Umer <nmanumr@gmail.com>
2019-03-11 22:58:38 +0500

    Create q5.c

---
+++ labsheet2/q5.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void q5()
+{
+    int i;
+    for (i = 1; i <= 100; i++)
+        printf("%d\t", i);
+
+    printf("\n");
+    for (i = 100; i >= 1; i--)
+        printf("%d\t", i);
+
+    printf("\n");
+    for (i = 20; i >= 2; i -= 2)
+        printf("%d\t", i);
+
+    printf("\n");
+    for (i = 2; i <= 20; i += 3)
+        printf("%d\t", i);
+
+    printf("\n");
+    for (i = 99; i >= 0; i -= 11)
+        printf("%d\t", i);
+}
