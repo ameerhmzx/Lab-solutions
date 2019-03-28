3319b250190e45cce022bab1e0c154d98155ddbc
Nauman Umer <nmanumr@gmail.com>
2019-03-28 12:50:44 +0500

    Update q2.c

--- labsheet4/q2.c
+++ labsheet4/q2.c
@@ -3,17 +3,24 @@
 #include <time.h>
 
 int randR(int min, int max){
-    srand(time(0));
-    return (rand() % (max - min + 1)) + max;
+    return (rand() % (max - min + 1)) + min;
 }
 
-void main()
+int main()
 {
-    int num1 = randR(1, 2);
-    int num2 = randR(1, 100);
-    int num3 = randR(0, 9);
-    int num4 = randR(1000, 1112);
-    int num5 = randR(-1, 1);
-    int num6 = randR(-3, 11);
-    printf("%d\n%d\n%d\n%d\n%d\n%d\n\n\n", num1, num2, num3, num4, num5, num6);
+    srand(time(0));
+    int num = randR(1, 2);
+    printf("%d\n", num);
+    num = randR(1, 100);
+    printf("%d\n", num);
+    num = randR(0, 9);
+    printf("%d\n", num);
+    num = randR(1000, 1112);
+    printf("%d\n", num);
+    num = randR(-1, 1);
+    printf("%d\n", num);
+    num = randR(-3, 11);
+    printf("%d\n", num);
+
+    return 0;
 }
