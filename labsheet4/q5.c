bb774bb2692cd28b2de8e5942c2ab493b85b938c
Nauman Umer <nmanumr@gmail.com>
2019-04-02 11:49:22 +0500

    Update q5.c

--- labsheet4/q5.c
+++ labsheet4/q5.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
-void main()
-{
-  // Will be available soon
+int GCD(int x, int y){
+  if (y==0) return x;
+  return GCD(y, x % y);
+}
+
+int main(void) {
+  printf("%d", GCD(12, 3));
+  return 0;
 }
