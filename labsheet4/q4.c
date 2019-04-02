1e5bf12f025c8936fb9a930c894e5d4aa5a5c902
Nauman Umer <nmanumr@gmail.com>
2019-04-02 12:18:06 +0500

    Update q4.c

--- labsheet4/q4.c
+++ labsheet4/q4.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 
-void main()
-{
-  // Will be available soon
+float F2C(float f){
+  return (f - 32) * 5 / 9;
+}
+
+float C2F(float c){
+  return (c * 9/5) + 32;
+}
+
+int main(void) {
+  printf("C\tF\n");
+  for(int i =0; i <= 100; i++){
+    printf("%d\t%.2f\n", i, C2F((float)i));
+  }
+
+  printf("\n\nF\tC\n");
+  for(int i =32; i <= 212; i++){
+    printf("%d\t%.2f\n", i, F2C((float)i));
+  }
+  return 0;
 }
