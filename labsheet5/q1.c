9b00a0d9549c44e49b3d3ff01b768afb3636578b
Nauman Umer <nmanumr@gmail.com>
2019-04-09 11:59:10 +0500

    Update q1.c

--- labsheet5/q1.c
+++ labsheet5/q1.c
@@ -1,6 +1,17 @@
-#include <stdio.h>
+#include<stdio.h>
+#define SUB 10
 
-void main()
-{
-  // Will be available soon
+void main(){
+    int marks;
+    int i, sum=0;
+
+    for(i=0; i < SUB; i++){
+        printf("Enter subject marks: ");
+        scanf("%d", &marks);
+        sum += marks;
+    }
+
+    printf("Sum: %d\n", sum);
+    printf("Percentange: %.2f\n", (float) sum / (100.0 * SUB) * 100.0);
+    printf("Average: %.2f\n", (float) sum / (float) SUB);
 }
