7471f59badbf451bc6fd0c2c3fe1b13ab8f44e8f
hassanarifbinarif <46750893+hassanarifbinarif@users.noreply.github.com>
2019-04-09 12:17:22 +0500

    Update q3.c

--- labsheet5/q3.c
+++ labsheet5/q3.c
@@ -1,6 +1,22 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<stdlib.h>
 
-void main()
+int main()
 {
-  // Will be available soon
+    int i;
+    float hrs[5],hrs_rate[5],sal;
+
+    for(i = 0;i < 5;i++)
+    {
+        printf("Enter the number of hours worked for employee %d: ",i+1);
+        scanf("%f",&hrs[i]);
+
+        printf("Enter the rate per hour worked for employee %d: ",i+1);
+        scanf("%f",&hrs_rate[i]);
+
+        printf("The basic salary of %d employee is %.2f\n",i+1,hrs[i]*hrs_rate[i]);
+    }
+
+    system("pause");
+    return 0;
 }
