149b15e0b423fdbd2c030b4f2d298014668833f7
Nauman Umer <nmanumr@gmail.com>
2019-05-07 10:26:53 +0500

    Update q3.c

--- labsheet7/q3.c
+++ labsheet7/q3.c
@@ -15,7 +15,36 @@ void strFreq(void) {
   }
 }
 
+void getStat(void) {
+  char str[10] = "asdfasdfgh";
+  int freq[4] = {0};
+  int i=0;
+
+  for(; i < strlen(str); i++){
+    if(str[i] == ' ')
+      freq[3]++;
+    
+    else if(isdigit(str[i]))
+      freq[2]++;
+    
+    if(str[i] == 'A' || str[i] == 'E' || str[i] == 'I'
+    || str[i] == 'O' || str[i] == 'U'
+    || str[i] == 'a' || str[i] == 'e' || str[i] == 'i'
+    || str[i] == 'o' || str[i] == 'u' )
+      freq[0]++;
+    else
+      freq[1]++;
+  }
+
+  printf("Digits: %d\n", freq[2]);
+  printf("Vowels: %d\n", freq[0]);
+  printf("Consonants: %d\n", freq[1]);
+  printf("Spaces: %d\n", freq[3]);
+
+}
+
 void main()
 {
   strFreq();
+  getStat();
 }
