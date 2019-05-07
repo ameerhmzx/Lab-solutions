9c3ce16879d3a611714625fbe87ff720c1538bca
Nauman Umer <nmanumr@gmail.com>
2019-05-07 10:16:24 +0500

    Update q3.c

--- labsheet7/q3.c
+++ labsheet7/q3.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#include <string.h>
+
+void strFreq(void) {
+  char str[10] = "asdfasdfgh";
+  int freq[256] = {0};
+  int i=0;
+
+  for(; i < strlen(str); i++){
+    freq[str[i]]++;
+  }
+
+  for(i=0; i < strlen(str); i++){
+    printf("%c: %d\n", str[i], freq[str[i]]);
+  }
+}
 
 void main()
 {
-  // Will be available soon
+  strFreq();
 }
