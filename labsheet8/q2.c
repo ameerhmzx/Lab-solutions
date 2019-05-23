811e81c8e105c4ec1b4d98d0c841290db8ff9edd
Ameer Hamza Naveed <31469896+blacklistedx@users.noreply.github.com>
2019-05-23 10:54:48 +0500

    Update q2.c

--- labsheet8/q2.c
+++ labsheet8/q2.c
@@ -2,5 +2,16 @@
 
 void main()
 {
-  // Will be available soon
+   FILE *f1ptr = fopen("file1.txt","r");
+  FILE *f2ptr = fopen("file2.txt","w");
+  if(f1ptr == NULL || f2ptr == NULL)
+  {
+    printf("\nUnable to open/create file!!!");
+    exit(0);
+  }
+  while(!feof(f1ptr)){
+    fputc(fgetc(f1ptr), f2ptr);
+  }
+  fclose(f1ptr);
+  fclose(f2ptr);
 }
