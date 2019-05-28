1a3c4b757ef8e9603b6e7aab059afd59ca014741
Nauman Umer <nmanumr@gmail.com>
2019-05-28 10:33:38 +0500

    Update q3.c

--- labsheet8/q3.c
+++ labsheet8/q3.c
@@ -1,6 +1,38 @@
 #include <stdio.h>
+#include <ctype.h>
 
-void main()
-{
-  // Will be available soon
+typedef struct {
+  int spaces;
+  int lines;
+  int words;
+  int character;
+  int tabs;
+} state;
+
+int main(void) {
+  FILE *fp = fopen("test", "r");
+  state state = {0, 1, 1, 0, 0};
+  char last;
+
+  while(!feof(fp)){
+    char ch = fgetc(fp);
+    if (ch == ' ')
+      state.spaces++;
+    else if(ch == '\t')
+      state.tabs++;
+    else if(ch == '\n')
+      state.lines++;
+    else if(last == ' ' && isalpha(ch))
+      state.words++;
+
+    state.character++;
+    last = ch;
+  }
+
+  printf("Characters: %d\n", state.character);
+  printf("Spaces: %d\n", state.spaces);
+  printf("Tabs: %d\n", state.tabs);
+  printf("Lines: %d\n", state.lines);
+  printf("Words: %d\n", state.words);
+  return 0;
 }
